06c3dd13028f616f40c8c584fcac9d83b222d9a0
Jesse Tipton <me@jessetipton.com>
2017-03-07 19:00:49 -0600

    Automated commit

---
+++ 7/FlappyBird/FlappyBird/FlappyScene.swift
@@ -0,0 +1,13 @@
+//
+//  FlappyScene.swift
+//  FlappyBird
+//
+//  Created by Jesse Tipton on 3/7/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+import SpriteKit
+
+class FlappyScene: SKScene {
+
+}
