2dd9d09a6a2c9541ef642365a77f5dd652c15323
Jesse Tipton <me@jessetipton.com>
2017-03-07 19:22:11 -0600

    Automated commit

--- 7/FlappyBird/FlappyBird/FlappyScene.swift
+++ 7/FlappyBird/FlappyBird/FlappyScene.swift
@@ -8,6 +8,14 @@
 
 import SpriteKit
 
+extension CGRect {
+    
+    var center: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+    
+}
+
 class FlappyScene: SKScene {
 
     private let birdNode: SKSpriteNode = {
